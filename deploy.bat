32e5b9d05bba06e258a4f82ab95f222ee1ab1106
unknown <luukhanhtung2002@gmail.com>
2020-11-03 18:58:58 +0700

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -4,6 +4,7 @@ CALL npm run build
 CALL cd ..
 CALL cd web-project-backend
 CALL RD D:\Web-project-backend\build /S /Q
+CAll git checkout heroku
 CALL mkdir build
 CALL xcopy D:\web-project\build D:\Web-project-backend\build /E
 PAUSE
